84cd6ec41729a554747a82a6f38b5d0898d63de6
Sergei Malevannyi <sergey.malevanny@gmail.com>
2022-10-14 15:09:14 +0300

    Lesson 2: Solution for task #6

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,4 @@
-import pytest
-from fixture.application import Application
 from model.contact import Contact
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     app.session.login(username="Admin", password="secret")
